75b818120e49f2b4f42880935adbea43f5cf7a74
bohrzhang <bohrzhang.coder@gmail.com>
2023-06-17 22:57:55 +0800

    assembly

---
+++ assembly/assembly/index.ts
@@ -0,0 +1,33 @@
+
+// Array
+export function arrAdd(nums: Array<i32>): Array<i32> {
+  const result: Array<i32> = nums.map((num: i32): i32=> num + 1)
+  return result
+}
+
+// Object
+class ComplexObject {
+  constructor() {} // !
+  field: string | null;
+}
+
+export function newObject(): ComplexObject {
+  return new ComplexObject();
+}
+
+export function setObjectField(target: ComplexObject, field: string | null): void {
+  target.field = field;
+}
+
+export function getObjectField(target: ComplexObject): string | null {
+  return target.field;
+}
+
+
+// binding js function
+@external("./binding.js", "myFunction")
+declare function myFunction(): string;
+
+export function runMyFunction(): string {
+  return myFunction()
+}
