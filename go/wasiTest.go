19ac8f46cc1c5b34747469855d90d5ca7d5366db
bohrzhang <bohrzhang.coder@gmail.com>
2023-07-03 23:27:12 +0800

    add wasmtime

---
+++ go/wasiTest.go
@@ -0,0 +1,10 @@
+package wasiTest
+
+//go:wasm-module yourmodulename
+//export add
+func add(x, y uint32) uint32 {
+	return x + y
+}
+
+// main is required for the `wasi` target, even if it isn't used.
+func wasiTest() {}
